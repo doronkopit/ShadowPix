1197c44dde1067a49e64b2d8ba835a54018b6df1
Yariv Levy <yalevy@paypal.com>
2020-12-18 16:39:34 +0200

    Some progress

--- blend/run_blend.py
+++ blend/run_blend.py
@@ -2,20 +2,21 @@ import os
 import sys
 from functools import partial
 from multiprocessing.dummy import Pool
-from subprocess import call
+from subprocess import call, check_output
 import glob
 import argparse
 import imageio
 from PIL import Image
 from pathlib import Path
 
-project_root = Path.cwd() 
+project_root = Path.cwd()
+project_root = str(project_root) 
 parser = argparse.ArgumentParser()
-parser.add_argument('--blender_path', type=str, default='/args/blender/blender', help='')
-parser.add_argument('--script_path', type=str, default=project_root / '/visualize/render.py', help='')
-parser.add_argument('--blend_path', type=str, default=project_root / '/visualize/stage2.blend', help='')
-parser.add_argument('--input_folder', type=str, default=project_root / '/models/mies.obj', help='')
-parser.add_argument('--output_folder', type=str, default=project_root / '/renders', help='')
+parser.add_argument('--blender_path', type=str, default='blender', help='')
+parser.add_argument('--script_path', type=str, default=project_root + '/blend/render.py', help='')
+parser.add_argument('--blend_path', type=str, default=project_root + '/blend/stage2.blend', help='')
+parser.add_argument('--input_folder', type=str, default=project_root + '/blend/local_output333.obj', help='')
+parser.add_argument('--output_folder', type=str, default=project_root + '/renders', help='')
 parser.add_argument('--gif_frames', type=int, default=0, help='number of gif frames')
 parser.add_argument('--duration', type=int, default=4, help='duration of gif in seconds')
 args = parser.parse_args()
@@ -23,6 +24,7 @@ args = parser.parse_args()
 
 def render(input_obj_dir, output_folder):
     print('Generating rendering commands...')
+    print(f'input_obj_dir={input_obj_dir}')
     if os.path.isfile(input_obj_dir):
         fullfile = input_obj_dir
     else:
@@ -91,4 +93,5 @@ def create_gif_global(base_name):
 
 if __name__ == "__main__":
     args.gif_frames = 40
-    create_gif_local('faces')
+    render(input_obj_dir=args.input_folder, output_folder=args.output_folder)
+    # create_gif_local('faces')
