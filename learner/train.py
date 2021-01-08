9d36a0aae3aae72685e5aaf2610abeb15a081509
Yariv Levy <yalevy@paypal.com>
2021-01-08 12:29:08 +0200

    More learning

--- learner/train.py
+++ learner/train.py
@@ -7,19 +7,17 @@ from learner.global_method_learning import GlobalMethodLearner
 from learner.learning_utils import print_stats
 import image_util
 
-pics = ["pics/pic_a.jpg", "pics/pic_b.jpg", "pics/pic_c.jpg", "pics/pic_d.jpg"]
-output = 'global_init_test2.obj'
-output_size = 200  # size in milemeters of output print
-wall_size = 0.25  # thickness of walls
-pixel_size = 2.5
-grid_size = int(output_size / (wall_size + pixel_size))
+if __name__ == '__main__':
+    pics = ["pics/pic_a.jpg", "pics/pic_b.jpg", "pics/pic_c.jpg", "pics/pic_d.jpg"]
+    output = 'global_init_test2.obj'
+    output_size = 200  # size in milemeters of output print
+    wall_size = 0.25  # thickness of walls
+    pixel_size = 2.5
+    grid_size = int(output_size / (wall_size + pixel_size))
+    res=1
 
-local = False
-res=1
-
-square_imgs = [image_util.load_pic_to_square_np(pic, output_size // res) for pic in pics]
-global_m = GlobalMethodLearner(square_imgs, output, output_size,steps=2*10**2,height_field_size=1)
-print("starting optimize")
-global_m.produce_pix()
-print_stats(global_m.metrices)
-
+    square_imgs = [image_util.load_pic_to_square_np(pic, output_size // res) for pic in pics]
+    global_m = GlobalMethodLearner(square_imgs, output, output_size,steps=2*10**2,height_field_size=1)
+    print("Strat training")
+    global_m.produce_pix()
+    print_stats(global_m.metrices)
